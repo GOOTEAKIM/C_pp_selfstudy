d1b11d921239f0ce8f94d5e71eb26febe3a7333a
김구태 <gootae0220@naver.com>
2024-11-07 00:56:58 +0900

    Study 7_4, 8_1

---
+++ C++_algo/C++_algo/b_31831.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	freopen("input.txt", "r", stdin);
+
+	int n, m;
+
+	cin >> n >> m;
+
+	int arr[];
+
+
+
+
+	return 0;
+}
---
+++ coding_panda_basic/section8/section8/lec8_1.cpp
@@ -0,0 +1,25 @@
+#include "struct.h"
+#include "new.h"
+int main() {
+
+	MyStruct Pandacoding = {
+		"panda",
+		26,
+	};
+
+	display(Pandacoding);
+
+	return 0;
+}
+
+/*
+
+헤더 파일
+	함수 원형
+	#define이나 const를 사용하는 기호 상수
+	구조체 선언
+	클래스 선언
+	템플릿 선언
+	인라인 함수
+
+*/
